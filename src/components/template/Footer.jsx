40d91691234bec63968333ded2d86859059ac000
Luiz <luiz.enz@accountfy.com>
2021-04-30 17:30:35 -0300

    first commit

---
+++ src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+            <strong> Papai<span className="text-danger">P</span>ig</strong>
+        </span>
+    </footer>
